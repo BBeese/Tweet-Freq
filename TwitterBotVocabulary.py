c282a18344e7a3234fc1536b401c0547131b6d1b
Brett B <36874395+BBeese@users.noreply.github.com>
2018-10-01 19:27:27 -0500

    Added comments, continued splitting the script into more easily readable funtions

--- TwitterBotVocabulary.py
+++ TwitterBotVocabulary.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 
 
 def string_cleaning(s):
+    """
+    :param s: String
+    :return: a 'clean' version of s, containing only lowercase letters.
+    """
+
     string = ""
     for char in s.lower():
         if (ord(char) > 122) or ((ord(char) < 97) and (char != "'")) or ("https://" in s):
@@ -16,6 +21,11 @@ def string_cleaning(s):
 
 
 def authenticate():
+    """
+    :return: Verified object
+    Authenticates credentials against Twitter's services.
+    """
+
     app_key = "ikZS64uOU2yVSsvteK3PQqxLc"
     app_secret = "TYEeT3cZDlvvs1nqUnlb6rTOLlESMLCJacDGbCZVCr7fjIR858"
     auth_token = "452468893-MN2lZaifrqXPe6kl9YLScKhncnexJZ6wazccfgma"
@@ -30,6 +40,14 @@ def authenticate():
 
 
 def graph(word_count, words, values, handle):
+    """
+    :param word_count: Int; how many words are graphed on the x-axis
+    :param words: List of Strings; words being plotted on x-axis
+    :param values: List of Ints; counts of each corresponding words in 'words' ^
+    :param handle: String; Title of the graph, the user's twitter handle
+    :return:
+    """
+
     plt.figure(figsize=(7, 7))
     plt.style.use("seaborn-white")
     plt.xticks(word_count, words, rotation="vertical", size=10, fontname="Arial")
@@ -42,25 +60,46 @@ def graph(word_count, words, values, handle):
     return
 
 
-def main():
-    api = authenticate()
-    handle = str(input("Enter the user's twitter handle: @"))
-    raw_tweets = []
-
+def pull_tweets(api, handle):
+    """
+    Gets a specified number of tweets from the Twitter user's account.
+    :param api: Twitter verification object
+    :param handle: String; A Twitter account's address
+    :return: List; The past 3000 Tweets from this userr
+    """
+    tweets = []
     for tweet in tweepy.Cursor(api.user_timeline, screen_name=handle).items(3000):
         # Make sure tweet isn't a retweet
         if tweet.text[0:2] != "RT":
-            raw_tweets.append(tweet.text)
+            tweets.append(tweet.text)
 
+    return tweets
+
+
+def count_words(tweets):
+    """
+    Populates a dictionary with words from the tweets, counts the nuuber of instances of each word
+    :param tweets: List; list of each tweet from pull_tweets
+    :return: dictionary with int values for each string key
+    """
     word_dict = {}
-    for tweets in raw_tweets:
-        tweets = tweets.lower().split()
-        for word in tweets:
-            word = string_cleaning(word)
-            if word in word_dict:
-                word_dict[word] += 1
+    for tweets in tweets:
+        clean_tweets = tweets.lower().split()
+        for word in clean_tweets:
+            w = string_cleaning(word)
+            if w in word_dict:
+                word_dict[w] += 1
             else:
-                word_dict[word] = 1
+                word_dict[w] = 1
+
+    return word_dict
+
+
+def main():
+    api = authenticate()
+    handle = str(input("Enter the user's twitter handle: @"))
+    raw_tweets = pull_tweets(api, handle)
+    word_dict = count_words(raw_tweets)
 
     word_list = []
     count = 0
